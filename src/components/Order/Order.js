e93ca355b6c3ec32e00620090d897324be2e327d
El attari Yassine <yassineattari64@gmail.com>
2020-06-02 17:53:55 +0100

    fix: navigation bug on orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -3,10 +3,12 @@ import React from 'react';
 import classes from './Order.module.css';
 
 const order = (props) => {
-    <div className={classes.Order}>
-        <p>Ingredients: Salad(1)</p>
-        <p>Price: <strong>USD 5.45</strong></p>
-    </div>
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: Salad(1)</p>
+            <p>Price: <strong>USD 5.45</strong></p>
+        </div>
+    );
 }
 
 export default order;
