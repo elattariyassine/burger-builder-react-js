26513c44748c74a20c33dd8fbece4f995b0b8452
El attari Yassine <yassineattari64@gmail.com>
2020-06-15 15:50:59 +0100

    feat: navigating back & next on controls without passing state

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -10,10 +10,17 @@ class Checkout extends React.Component {
             cheese: 1
         }
     }
+    checkoutCancelledHandler = () => {
+        this.props.history.goBack();
+    }
+    checkoutContinuedHandler = () => {
+        this.props.history.replace('/checkout/contact-data');
+    }
     render(){
         return (
             <div>
-                <CheckoutSummary ingredients={this.state.ingredients}/>
+                <CheckoutSummary ingredients={this.state.ingredients} 
+                checkoutCancelled={this.checkoutCancelledHandler} checkoutContinued={this.checkoutContinuedHandler}/>
             </div>
         );
     }
