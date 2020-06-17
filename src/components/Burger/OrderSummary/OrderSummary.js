d20bc64081fd93df6075f5c7bde2ba8b1930af53
El attari Yassine <yassineattari64@gmail.com>
2020-06-17 23:10:58 +0100

    feat: improve performance by changing modal & orderSummary into class base components and control rendering via shoulComponentUpdate...

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,27 +2,34 @@ import Aux from '../../../hoc/Auxiliary';
 import React from 'react';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const style = {
-        textTransform: 'capitalize'
+class OrderSummary extends React.Component{
+
+    componentDidUpdate(){
+        console.log('[OrderSummary] will update');        
+    }
+
+    render(){
+        const style = {
+            textTransform: 'capitalize'
+        };
+        const ingredientSummary = Object.keys(this.props.ingredients)
+                .map(igKey => {
+                    return <li key={igKey}><span style={style}>{igKey}</span>: {this.props.ingredients[igKey]}</li>
+                });
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        );
     };
-    const ingredientSummary = Object.keys(props.ingredients)
-            .map(igKey => {
-                return <li key={igKey}><span style={style}>{igKey}</span>: {props.ingredients[igKey]}</li>
-            });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    );
 };
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Model.js
+++ src/components/UI/Modal/Model.js
@@ -3,14 +3,27 @@ import classe from './Modal.module.css';
 import Aux from '../../../hoc/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div className={classe.Modal} 
-            style={{transform: props.show ? 'translateY(0)' : 'translateY(-100vh)', opacity: props.show ? '1' : '0'}}>
-                {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends React.Component{
 
-export default modal;
+    shouldComponentUpdate(nextProps, nextState){
+        return nextProps.show !== this.props.show;
+    }
+
+    componentWillUpdate(){
+        console.log('[Modal] willUpdate');
+    }
+
+    render(){
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className={classe.Modal} 
+                    style={{transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)', opacity: this.props.show ? '1' : '0'}}>
+                        {this.props.children}
+                </div>
+            </Aux>
+        );
+    }
+};
+
+export default Modal;
