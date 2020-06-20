df27310ac15890ca817a5ca3083998bf2e94405c
El attari Yassine <yassineattari64@gmail.com>
2020-06-20 15:25:21 +0100

    feat: added checkout container

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './Checkout.module.css';
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+
+const checkoutSummary = (props) => {
+    return (
+        <div className={classes.CheckoutSummary}>
+            <h1>We hope it tastes well!</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Burger ingredients={props.ingredients}/>
+            </div>
+            <Button btnType="Danger">CANCEL</Button>
+            <Button btnType="Success">CONTINUE</Button>
+        </div>
+    );
+}
+
+export default checkoutSummary;
