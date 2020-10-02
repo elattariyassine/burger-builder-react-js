6b8e6e887ba84033346170f74a2c94687d1d4330
ELATTARIYassine <yassineattari64@gmail.com>
2020-10-02 16:21:01 +0000

    feat: added auth form & auth actions

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import Layout from './components/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import {Route, Switch} from 'react-router-dom';
-import Orders from './containers/Orders/Orders';
+import React, { Component } from "react";
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
+import { Route, Switch } from "react-router-dom";
+import Orders from "./containers/Orders/Orders";
+import Auth from "./containers/Auth/Auth";
 
 class App extends Component {
-  render(){
+  render() {
     return (
       <div>
         <Layout>
           <Switch>
-            <Route path="/checkout" component={Checkout}/>
-            <Route path="/orders" component={Orders}/>
-            <Route path="/" exact component={BurgerBuilder}/>
+            <Route path="/auth" component={Auth} />
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
       </div>
